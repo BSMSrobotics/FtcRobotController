8565a840ee7895c8192a0fd4e96653696ea8d44e
BSMSrobotics <chris.lang@bsmsrobotics.org>
2026-07-26 11:20:50 -0400

    Create ServoExamples.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoExamples.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.core.widget.TextViewCompat;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Mechanisms.TestBenchServo;
+
+@TeleOp
+public class ServoExamples extends OpMode {
+    TestBenchServo bench = new TestBenchServo();
+    double leftTrigger, rightTrigger;
+
+    @Override
+    public void init(){
+        bench.init(hardwareMap);
+        leftTrigger = 0.0;
+        rightTrigger = 0.0;
+    }
+
+    @Override
+    public void loop() {
+        leftTrigger = gamepad1.left_trigger;
+        rightTrigger = gamepad1.right_trigger;
+
+        bench.setServoRot(rightTrigger);
+        bench.setServoPos(leftTrigger);
+
+    }
+}
