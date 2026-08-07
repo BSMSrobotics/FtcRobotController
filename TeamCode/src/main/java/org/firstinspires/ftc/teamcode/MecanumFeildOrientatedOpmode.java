991930cb0d3f3ce1718ac77c535c3a07396534be
BSMSrobotics <chris.lang@bsmsrobotics.org>
2026-08-07 19:49:33 -0400

    Update MecanumFeildOrientatedOpmode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFeildOrientatedOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFeildOrientatedOpmode.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.MecanumDrive;
 
@@ -10,19 +13,28 @@ public class MecanumFeildOrientatedOpmode extends OpMode {
     MecanumDrive drive = new MecanumDrive();
     double forward, strafe, rotate;
 
+    private DcMotor intake = null;
+    double intakePower;
+
+
     @Override
     public void init(){
        drive.init(hardwareMap);
+       intake = hardwareMap.get(DcMotorEx.class,"intake");
+       intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       intake.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
     @Override
     public void loop(){
-       forward = gamepad1.left_stick_y;
+       forward = -gamepad1.left_stick_y;
        strafe = gamepad1.left_stick_x;
        rotate = gamepad1.right_stick_x;
 
        drive.driveFieldRelative(forward,strafe,rotate);
+       intakePower = gamepad1.right_trigger - gamepad1.left_trigger;
+       intake.setPower(intakePower);
 
     }
 }
